40e22d82fbdc5471a9f5ca4d6cfd901275ff218e
Jorge Neto <jorgesegtowich@gmail.com>
2023-12-05 20:32:17 -0300

    start digimons structure

---
+++ src/app/pages/digimons/digimons.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-digimons',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './digimons.component.html',
+  styleUrls: ['./digimons.component.scss']
+})
+export class DigimonsComponent {
+
+}
