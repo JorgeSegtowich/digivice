d6af53783de907a2dabd25bb4185bd707746abff
Jorge Neto <jorgesegtowich@gmail.com>
2023-12-05 22:54:30 -0300

    avanço no digimon component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { DigimonsComponent } from './pages/digimons/digimons.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // { path: '/', component: DigimonsComponent },
+    { 
+        path: 'digimons', 
+        // component: DigimonsComponent 
+        loadComponent: () => import('./pages/digimons/digimons.component').then(c => c.DigimonsComponent)
+    },
+    { path: '',   redirectTo: '/digimons', pathMatch: 'full' }, // redirect to `first-component`
+    // { path: '**', component: PageNotFoundComponent },  // Wildcard route for a 404 page
+];
--- src/app/pages/digimons/digimons.component.ts
+++ src/app/pages/digimons/digimons.component.ts
@@ -1,13 +1,38 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DigimonService } from '../../shared/services/digimon.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-digimons',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule],
+  providers: [DigimonService],
   templateUrl: './digimons.component.html',
   styleUrls: ['./digimons.component.scss']
 })
 export class DigimonsComponent {
 
+  digimons: Array<any> = [];
+
+  constructor(
+    private readonly digimonService: DigimonService
+    ) {
+    this.getDigimons();
+  }
+  
+  getDigimons() {
+    this.digimonService.getAllDigimons().subscribe(
+      (response: any) => {
+        console.log(response)
+        this.digimons = response.content;
+      },
+      (error) => {
+        console.log(error);
+      });
+  }
+
+  trackById(index: number, digimon: any): number {
+    return digimon.id; // Retorna a chave única para o item
+  }
 }
